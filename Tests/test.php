dc44065db7c8587831c0a3271a36be3f76e08b47
Matias Aguirre <maguirre@matware.com.ar>
2018-06-11 18:31:42 -0300

    Fix prefix

--- Tests/test.php
+++ Tests/test.php
@@ -14,14 +14,14 @@ use Joomla\Database\DatabaseFactory;
 use Joomla\Entity\Tests\Models\User;
 use Joomla\Entity\Tests\Models\Content;
 
-$options = array('user' => 'gsoc18_webservices', 'password' => 'gsoc18_webservices', 'database' => 'gsoc18_webservices');
-$attributes = array('table' => 'kf3n7_users');
+$options = array('user' => 'gsoc18_webservices', 'password' => 'gsoc18_webservices', 'database' => 'gsoc18_webservices', 'prefix' => 'kf3n7_');
+$attributes = array('table' => '#__users');
 
 $driver = DatabaseFactory::getDriver('mysql', $options);
 
 $modelUser = new User($driver, $attributes);
 $modelContent = new Content($driver);
 
-$modelUser->setTable('kf3n7_users');
+$modelUser->setTable('#__users');
 
 print_r($modelUser->find(948));
